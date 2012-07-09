670197563b7e23d619c52ffdfd8b24f4fdac0c3e
David Pfau <hotmail.com@gmail.com>
2012-07-09 01:54:22 -0400

    Made working form so that new fields may be created.

--- prototype/urls.py
+++ prototype/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('prototype.views',
     url(r'^$', 'index'),
-    url(r'^/create$', 'create')
-    url(r'^(?P<thread_title>\w+)/$', 'detail'),
+    url(r'^create/$', 'create'),
+    url(r'^thread/(?P<thread_title>\w+)/$', 'detail'),
     #url(r'^(?P<poll_id>\d+)/results/$', 'results'),
     #url(r'^(?P<poll_id>\d+)/vote/$', 'vote'),
 )
--- prototype/views.py
+++ prototype/views.py
@@ -1,11 +1,14 @@
 from prototype.models import Thread
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-#from django.http import Http404
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 def index(request):
 	thread_list = Thread.objects.all()
-	return render_to_response('prototype/index.html', {'thread_list': thread_list}, context_instance=RequestContext(request))
+	return render_to_response('prototype/index.html', 
+		{'thread_list': thread_list}, 
+		context_instance=RequestContext(request))
 
 def detail(request, thread_title):
 	try:
@@ -15,5 +18,19 @@ def detail(request, thread_title):
 	return render_to_response('prototype/detail.html', {'thread': t})
 
 def create(request):
-	t = Thread(title=request.POST['new_thread_name'].replace(' ','_'))
-	t.save()
+	thread_list = Thread.objects.all()
+	new_thread_name = request.POST['new_thread_name'].replace(' ', '_')
+	if len(new_thread_name) is 0:
+	 	return render_to_response('prototype/index.html', 
+	 		{'thread_list': thread_list, 'thread_exists': 'No name provided for thread'}, 
+	 		context_instance=RequestContext(request))
+	else:
+	 	if len(thread_list.filter(title=new_thread_name)) is 0:
+	 		t = Thread(title=new_thread_name)
+	 		t.save()
+	 		return HttpResponseRedirect(reverse('prototype.views.detail', args=(new_thread_name,)))
+	 	else:
+	 		return render_to_response('prototype/index.html', 
+	 			{'thread_list': thread_list, 'thread_exists': 'Thread with that name exists'}, 
+	 			context_instance=RequestContext(request))
+	# return render_to_response('prototype/create.html', {'new_thread_name': new_thread_name}, context_instance=RequestContext(request))
